7916766ffe686c823c1bc6d0447e0863aa1135ca
Revi Siva <rvlambda@gmail.com>
2025-01-19 23:28:57 +0530

    Update index.py
    Fixed server errors

--- api/index.py
+++ api/index.py
@@ -1,18 +1,35 @@
-import json
 from flask import Flask, request, jsonify
+import json
+import traceback
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
+# Load JSON data from file
+try:
+    with open('\public\q-vercel-python.json') as f:
+        data = json.load(f)
+except Exception as e:
+    print(f"Error loading JSON: {e}")
+
+@app.route('/get_marks', methods=['GET'])
 def get_marks():
     names = request.args.getlist('name')
+    if not names:
+        return jsonify({"error": "Name parameter is missing"}), 400
+
+    results = ""
+    for name in names:
+        result = next((item for item in data if item['name'] == name), None)
+        if result:
+            #results.append(result['marks'])
+            results += str(result['marks']) + " "
     
-    with open('public/q-vercel-python.json', 'r') as file:
-        data = json.load(file)
     
-    marks = {name: data.get(name, 0) for name in names}
-    
-    return jsonify({"Marks": marks})
+    if results:
+        results = results.strip()
+        return jsonify({"marks": results}), 200
+    else:
+        return jsonify({"error": "Names not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
