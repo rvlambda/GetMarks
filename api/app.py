88fe5438af8ba9ad059cf530c731a076c9f458f1
Revi Siva <rvlambda@gmail.com>
2025-01-20 09:04:04 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -7,7 +7,13 @@ def home():
     names = request.args.getlist('name')
     if not names:
         return jsonify({"error": "Name parameter is missing"}), 400
-    return names
+    #return names
+    results = "marks: 10 20"
+    if results:
+        results = results.strip()
+        return jsonify({"marks": results}), 200
+    else:
+        return jsonify({"error": "Names not found"}), 404
 
 @app.route('/about')
 def about():
