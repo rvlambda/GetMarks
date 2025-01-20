9fe7b9aded00fe19b4fae6c54f3a769f5ceb4595
Revi Siva <rvlambda@gmail.com>
2025-01-20 08:59:53 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     names = request.args.getlist('name')
+    if not names:
+        return jsonify({"error": "Name parameter is missing"}), 400
     return names
 
 @app.route('/about')
