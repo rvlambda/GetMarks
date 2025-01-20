13c8d9e22b53a1c0ac037975b1dddbaaeb710abf
Revi Siva <rvlambda@gmail.com>
2025-01-20 08:50:54 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,36 +1,11 @@
-from flask import Flask, request, jsonify
-import json
-import traceback
-from os.path import join
+from flask import Flask
 
 app = Flask(__name__)
 
-# Load JSON data from file
-try:
-    with open(join('public', 'q-vercel-python.json'),'r') as f:
-        data = json.load(f)
-except Exception as e:
-    print(f"Error loading JSON: {e}")
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-@app.route('/api', methods=['GET'])
-def get_marks():
-    names = request.args.getlist('name')
-    if not names:
-        return jsonify({"error": "Name parameter is missing"}), 400
-
-    results = ""
-    for name in names:
-        result = next((item for item in data if item['name'] == name), None)
-        if result:
-            #results.append(result['marks'])
-            results += str(result['marks']) + " "
-    
-    
-    if results:
-        results = results.strip()
-        return jsonify({"marks": results}), 200
-    else:
-        return jsonify({"error": "Names not found"}), 404
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/about')
+def about():
+    return 'About'
